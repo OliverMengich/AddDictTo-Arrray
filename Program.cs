3d45655e7cb0ab1cc82f2d1fa0c3eeb85ec31100
Oliver.Mengich <kemeioliver2000@gmail.com>
2021-01-12 09:06:58 +0300

    Add array to dictionary

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAppend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chain = new List<string>();
+
+            var block = new Dictionary<string, string>();
+            block.Add("Olive", "kem");
+            block.Add("Kemei", "skinny");
+            block.Add("Tim","Corey");
+
+            foreach (var item in block)
+            {
+                Console.WriteLine(item.Key+ ":" + item.Value);
+            }
+            
+
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
